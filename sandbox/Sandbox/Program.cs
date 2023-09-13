d6d210d4b2e9449957ac27fae94af16f3cb7ece4
joy935 <tupaiv@gmail.com>
2023-09-13 09:16:12 +0200

    added favorite color

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!!");
+        Console.WriteLine("This is C#.");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
     }
 }
