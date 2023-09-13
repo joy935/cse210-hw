d48e218f82f1d5ba64e56d6df1f9f197b3506221
joy935 <tupaiv@gmail.com>
2023-09-13 09:27:20 +0200

    added a whi te line

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
-
+        Console.WriteLine("");
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
