fa3ac0ec8bf6ed09bc884c330ed2db6776393490
joy935 <tupaiv@gmail.com>
2023-09-13 10:14:58 +0200

    get grade and convert it to int

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeUser = Console.ReadLine();
+        int grade = int.Parse(gradeUser);
+        Console.WriteLine($"Your grade is {grade}");
     }
 }
