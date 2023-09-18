c779de8f50e4a92ae6c2837eb47a7681855c4231
joy935 <tupaiv@gmail.com>
2023-09-18 17:56:12 +0200

    added random magic number

--- csharp-prep/Prep2/guessnumber.cs
+++ csharp-prep/Prep2/guessnumber.cs
@@ -5,9 +5,12 @@ class Program
     static void Main(string[] args)
     {
         
-        Console.Write("What is your magic number? ");
-        string number = Console.ReadLine();
-        int magicNumber = int.Parse(number);
+        // Console.Write("What is your magic number? ");
+        // string number = Console.ReadLine();
+        // int magicNumber = int.Parse(number);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
 
         int guessNumber = 0;
 
