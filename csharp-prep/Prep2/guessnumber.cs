b4f2213311902a49ffb414dc434fd337a37a48f2
joy935 <tupaiv@gmail.com>
2023-09-18 16:24:35 +0200

    asked for magic number and stored as an int

---
+++ csharp-prep/Prep2/guessnumber.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your magic number? ");
+        string number = Console.ReadLine();
+        int magicNumber = int.Parse(number);
+
+    }
+}
