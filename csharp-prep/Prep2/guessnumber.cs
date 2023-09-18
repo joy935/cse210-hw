b73704e6402774f4a461dae60f4c230dc16ae6ba
joy935 <tupaiv@gmail.com>
2023-09-18 17:50:07 +0200

    added a loop to keep guessing

--- csharp-prep/Prep2/guessnumber.cs
+++ csharp-prep/Prep2/guessnumber.cs
@@ -4,14 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Console.Write("What is your magic number? ");
         string number = Console.ReadLine();
         int magicNumber = int.Parse(number);
 
-        Console.Write("What is your guess? ");
-        string guess = Console.ReadLine();
-        int guessNumber = int.Parse(guess);
+        int guessNumber = 0;
 
+        do {
+            Console.Write("What is your guess? ");
+            string guess2 = Console.ReadLine();
+            int guessNumber2 = int.Parse(guess2);
 
+            if (guessNumber2 == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+            else 
+            {
+                if (guessNumber2 > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else {
+                    Console.WriteLine("Higher");
+                }
+
+            }
+        } while (guessNumber != magicNumber);
+        
     }
 }
