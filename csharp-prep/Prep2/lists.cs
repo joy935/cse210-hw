54b89884e9084e0b87fb44c6e361ad82e565e5db
joy935 <tupaiv@gmail.com>
2023-09-19 15:40:32 +0200

    added find the smallest positive number and sort

--- csharp-prep/Prep2/lists.cs
+++ csharp-prep/Prep2/lists.cs
@@ -5,6 +5,53 @@ class Program
     static void Main(string[] args)
     {
 
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+
+        float number = 1;
+        List<float> numbers = new List<float>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = float.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        float sum = 0;
+        float largest = 0;
+        float smallest = 99999;
+        foreach (int oneNumber in numbers)
+        {
+            sum += oneNumber;
+            if (oneNumber > largest) 
+            {
+                largest = oneNumber;
+            }
+            else if (oneNumber < smallest && oneNumber > 0)
+            {
+                smallest = oneNumber;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float lenNumbers = (numbers.Count - 1);
+        float average = sum / lenNumbers;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+
+        numbers.RemoveAt(numbers.Count - 1);
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+
         
 
 
