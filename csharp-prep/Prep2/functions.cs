cfc37aa785cad32d977029a45e1eab673934024c
joy935 <tupaiv@gmail.com>
2023-09-19 16:44:27 +0200

    managed files

--- csharp-prep/Prep2/functions.cs
+++ csharp-prep/Prep2/functions.cs
@@ -5,11 +5,39 @@ class Program
     static void Main(string[] args)
     {
         DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int sNumber = SquareNumber(userNumber);
+        DisplayResult(userName, sNumber);
 
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }
 
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sNumber = (number * number);
+            return sNumber;
+        }
+
+        static void DisplayResult(string userName, int sNumber)
+        {
+            Console.Write($"{userName}, the square of your number is {sNumber}. ");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        float number = 1;
+        List<float> numbers = new List<float>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = float.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        float sum = 0;
+        int largest = 0;
+        int smallest = 99999;
+        foreach (int oneNumber in numbers)
+        {
+            sum += oneNumber;
+            if (oneNumber > largest) 
+            {
+                largest = oneNumber;
+            }
+            else if (oneNumber < smallest && oneNumber > 0)
+            {
+                smallest = oneNumber;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float lenNumbers = (numbers.Count);
+        float average = sum / lenNumbers;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
