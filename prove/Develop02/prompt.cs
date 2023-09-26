fb2fe6ba9a1d6be19d9a8eee695ae185094240a2
joy935 <tupaiv@gmail.com>
2023-09-26 16:33:21 +0200

    added prompts

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -4,9 +4,16 @@ public class Prompt
 {
     private static List<string> promptList = new List<string> 
     {
-        "test1",
-        "test2",
-        "test3",
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What have you done differently today?",
+        "What is something unexpected that happened today?",
+        "If you could change one thing from today, what would it be?",
+        "List 3 things that helped you feel happy today.",
+        "List 3 things you are grateful for.",
     };
 
     public void GetRandomPrompt()
