aaea2418191be893803de5b66d86ebffc16da272
joy935 <tupaiv@gmail.com>
2023-09-26 17:40:39 +0200

    added menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,16 +6,39 @@ partial class Program
     {
         Console.WriteLine("Welcome to the Journal!");
 
-        while (true)
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        string choiceUser = Console.ReadLine();
+        int choice = int.Parse(choiceUser);
+
+        if (choice == 1)
         {
-            Console.WriteLine("Please select one of the following choices:");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("2. Quit");
-            string choiceUser = Console.ReadLine();
-            int choice = int.Parse(choiceUser);    
+            Console.WriteLine("1");
+            //break;
+        }
+        else if (choice == 2)
+        {
+            Console.WriteLine("2");
+        //     break;
+        }
+        else if (choice == 3)
+        {
+            Console.WriteLine("3");
+        //     break;
+        }
+        else if (choice == 4)
+        {
+            Console.WriteLine("4");
+        //     break;
+        }
+        else if (choice == 5)
+        {
+            Console.WriteLine("5");
+        //     break;
         }
     }
 }
