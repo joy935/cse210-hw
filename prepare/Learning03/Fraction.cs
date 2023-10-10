a38aadf0d2dc57a05a998a9f315377917ca9d078
joy935 <tupaiv@gmail.com>
2023-10-10 09:49:54 +0200

    added getter and setter

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,6 +2,46 @@ using System;
 
 public class Fraction 
 {
-    private int _top = 1;
-    private int _bottom = 1;
+    private int _top;
+    private int _bottom;
+
+    // Constructor that has no parameters that initializes the number to 1/1.
+    public Fraction() 
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor that has one parameter for the top and that initializes the denominator to 1. So that if you pass in the number 5, the fraction would be initialized to 5/1.
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+    // Constructor that has two parameters, one for the top and one for the bottom.
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
 };
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(6,7);
+
+        f1.SetTop(5);
+        Console.WriteLine(f1.GetTop());
+        f2.SetTop(6);
+        f2.SetBottom(7);
+        Console.WriteLine(f2.GetTop());
+        Console.WriteLine(f2.GetBottom());
+        
     }
 }
