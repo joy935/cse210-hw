bacba865edd0717ffcac1c4a2410c9fd50a92ed9
joy935 <tupaiv@gmail.com>
2023-10-12 14:14:01 +0200

    added while loop to start

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        while (true)
+        {
+            Console.WriteLine("Scripture Memorizer Program");
+            Console.WriteLine("display ref and vers");
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish.");
+            string userChoice = Console.ReadLine();
+
+            if (userChoice == "quit")
+            {
+                break;
+            }
+            else if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+            }
+        }
+        
     }
 }
