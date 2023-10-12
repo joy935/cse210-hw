1be2eb208ec623e08223742ecd5d762eb1d875fc
joy935 <tupaiv@gmail.com>
2023-10-12 14:22:13 +0200

    created attributes methods and constructor

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,29 @@
+using System
+
+private string _reference;
+private string _text;
+
+public void HideWords()
+{
+    string[] words = _text.Split(' ');
+    Random random = new Random();
+    int randomIndex = random.Next(0, words.Length);
+    words[randomIndex] = "______";
+    _text = string.Join(" ", words);
+}
+
+public string GetRenderedText()
+{
+    return $"{_reference} {_text}";
+}
+
+public string IsCompletelyHidden()
+{
+    return _text.Contains("______");
+}
+
+public Scripture(string reference, string text)
+{
+    _reference = reference;
+    _text = text;
+}
