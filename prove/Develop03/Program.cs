d5e2cabc79f7215aea9064f853155aa3cd3e19fc
joy935 <tupaiv@gmail.com>
2023-10-18 11:05:18 +0200

    Scripture with multiple verses

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scripture scripture = new Scripture("John", "3", "16", "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        //Scripture scripture = new Scripture("John", "3", "16", "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+
+        Scripture scripture = new Scripture("Proverbs", "3", "5", "6", "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
 
         while (true)
         {
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -64,12 +64,27 @@ public class Scripture
     {
         reference = new Reference(book, chapter, verse, verseEnd);
         this._text = text;
+
+        // create a list from the text and split up the words
+        List<string> wordList = text.Split(" ").ToList();
+
+        // create a list of Word objects
+        wordObjects = new List<Word>();
+
+        // split up the words into strings to create Word objects
+        foreach (string word in wordList)
+        {
+            Word oneWord = new Word(word);
+            wordObjects.Add(oneWord);
+        }
     }
+
     public Reference Reference
     {
         get { return reference; }
         set { reference = value; }
     }
+    
     public string Text
     {
         get { return _text; }
