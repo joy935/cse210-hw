62fb116ad79ab377655a7ac6ade620ee8433ad94
joy935 <tupaiv@gmail.com>
2023-10-26 15:22:43 +0200

    created constructor and getprompt

--- prove/Develop04/ListeningActivity.cs
+++ prove/Develop04/ListeningActivity.cs
@@ -1,7 +1,29 @@
 using System;
 
 /* Listening Activity class */
-// public class ListeningActivity : Activity
-// {
+public class ListeningActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?",
+    };
 
-// }
+    public ListeningActivity(string nameActivity, string description) : base (nameActivity, description)
+    {
+        _nameActivity = nameActivity;
+        _description = description;
+    }
+
+    public string GetPrompt()
+    {
+        Random random = new Random();
+        int i = random.Next(_prompts.Count);
+        return _prompts[i];
+    }
+
+
+}
