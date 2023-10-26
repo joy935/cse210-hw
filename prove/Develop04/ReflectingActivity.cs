80064c7d42d0146094b7223aca73aa5c3b21aa1d
joy935 <tupaiv@gmail.com>
2023-10-26 13:17:39 +0200

    created attributes and constructor

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -3,5 +3,12 @@ using System;
 /* Reflecting Activity class */
 public class ReflectingActivity : Activity
 {
-    
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+
+    public ReflectingActivity(string nameActivity, string description) : base (nameActivity, description)
+    {
+        _nameActivity = nameActivity;
+        _description = description;
+    }
 }
