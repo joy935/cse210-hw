b134bf5e2a5cc0f7e09a07be700db9955d358333
joy935 <tupaiv@gmail.com>
2023-10-26 09:39:49 +0200

    created menu options

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfullness Program!");
+        
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listening activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine("Select a choice from the menu.");
+            string choiceUser = Console.ReadLine();
+            int choice = int.Parse(choiceUser);
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Starting breathing activity...");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Starting reflecting activity...");
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Starting listening activity...");
+            }
+            else if (choice == 4)
+            {
+                break;
+            }
+
+        }
     }
 }
