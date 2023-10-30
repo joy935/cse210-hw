a9e2a277c697ca9e5957b88d1790ba315b2c78d2
joy935 <tupaiv@gmail.com>
2023-10-30 13:30:26 +0100

    created affirmation activity class

---
+++ prove/Develop04/Affirmation.cs
@@ -0,0 +1,46 @@
+using System;
+
+/* Affirmation class */
+public class AffirmationActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "I feel surrounded by love everywhere.",
+        "I am a person of faith, and I believe that good things lie ahead of me!",
+        "I give myself permission to do what is right for me.",
+        "I forgive those who have harmed me in my past and peacefully detach from them.",
+        "I deserve to be happy and loved.",
+        "I give myself space to grow and learn.",
+        "I put my energy into things that matter to me.",
+        "My drive and ambition allow me to achieve my goals.",
+        "I accomplish everything I set my mind to.",
+        "Counting our blessings is far better than recounting our problems.",
+    }
+
+    public AffirmationActivity(string nameActivity, string description, int duration) : base (nameActivity, description, duration)
+    {
+        _nameActivity = nameActivity;
+        _description = description;
+        _duration = duration;
+    }
+
+    public string GetPrompt()
+    {
+        Random random = new Random();
+        int i = random.Next(_prompts.Count);
+        return _prompts[i];
+    }
+
+    public void DisplayOpeningMessage()
+    {
+        Console.WriteLine("Consider the following affirmation:\n");
+        Console.Write("You may begin in: ");
+        PauseCountdown();
+    }
+
+    public void DisplayPrompt()
+    {
+        string prompt = GetPrompt();
+        Console.WriteLine(prompt);
+    }
+}
