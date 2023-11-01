e4644256a8c6c444a7985548de98995344ee64ba
joy935 <tupaiv@gmail.com>
2023-11-01 10:08:25 +0100

    created shape class

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,26 @@
+using System;
+
+public virtual class Shape
+{
+    private string _color;
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void Shape(string color)
+    {
+        SetColor(color);
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
