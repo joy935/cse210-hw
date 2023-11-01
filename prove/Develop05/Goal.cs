21130accd9664e5c2ceb72fefa52f5588e18f270
joy935 <tupaiv@gmail.com>
2023-11-01 18:55:57 +0100

    created base class goal

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,51 @@
+using System;
+
+/* Goal Base Class */
+public class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    public int CreateGoal()
+    {
+        return -1;
+    }
+}
