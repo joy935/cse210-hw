ff7f9d2689f023baceef6228341dced9825aee57
joy935 <tupaiv@gmail.com>
2023-11-01 10:22:13 +0100

    added a list of shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,12 +6,18 @@ class Program
     {
 
         Square square = new Square("Red", 5);
-        Console.WriteLine($"Square - Color: {square.GetColor()} - Area: {square.GetArea()}");
+        Console.WriteLine($"Color: {square.GetColor()} - Area: {square.GetArea()}");
 
         Rectangle rectangle = new Rectangle("Blue", 5, 10);
-        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()} - Area: {rectangle.GetArea()}");
+        Console.WriteLine($"Color: {rectangle.GetColor()} - Area: {rectangle.GetArea()}");
 
         Circle circle = new Circle("Green", 5);
-        Console.WriteLine($"Circle - Color: {circle.GetColor()} - Area: {circle.GetArea()}");
+        Console.WriteLine($"Color: {circle.GetColor()} - Area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        
     }
 }
