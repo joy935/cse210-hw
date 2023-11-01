cc459dd2b9e5cfe8e1d0918465416644cab41783
joy935 <tupaiv@gmail.com>
2023-11-01 10:19:39 +0100

    tested rectangle class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,6 +6,9 @@ class Program
     {
 
         Square square = new Square("Red", 5);
-        Console.WriteLine($"Color: {square.GetColor()} - Area: {square.GetArea()}");
+        Console.WriteLine($"Square - Color: {square.GetColor()} - Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 10);
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()} - Area: {rectangle.GetArea()}");
     }
 }
