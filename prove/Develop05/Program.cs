4ed5b2af7ea6b897d0c738efbf92e5b9cbc56e01
joy935 <tupaiv@gmail.com>
2023-11-02 09:22:33 +0100

    added menu options

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Goal goal = new Goal("Goal", "Description", 10);
+        Console.WriteLine("You have ... points.\n");
+        Console.WriteLine("Menu Options: ");
+        Console.WriteLine(" 1. Create a new Goal");
+        Console.WriteLine(" 2. List Goals");
+        Console.WriteLine(" 3. Save Goals");
+        Console.WriteLine(" 4. Load Goals");
+        Console.WriteLine(" 5. Record Event");
+        Console.WriteLine(" 6. Quit");
+        Console.WriteLine("Select a choice from the menu: ");
+        string choice = Console.ReadLine();
+        
     }
 }
