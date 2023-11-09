b9cbc5957e4c0600d2b1c28cb3390139791b2dcb
joy935 <tupaiv@gmail.com>
2023-11-09 09:59:58 +0100

    white space

--- prove/Develop05/UserInterface.cs
+++ prove/Develop05/UserInterface.cs
@@ -10,18 +10,18 @@ public class UserInterface
         _goals = goals;
     }
 
-public int UpdateGoal(int goalIndex, List<Goal> goals)
-{
-    if (goalIndex >= 0 && goalIndex < goals.Count)
+    public int UpdateGoal(int goalIndex, List<Goal> goals)
     {
-        Goal goalToUpdate = goals[goalIndex];
-        goalToUpdate.RecordEvent(); // to update the existing goal
-        return goalToUpdate.GetPoints();
+        if (goalIndex >= 0 && goalIndex < goals.Count)
+        {
+            Goal goalToUpdate = goals[goalIndex];
+            goalToUpdate.RecordEvent(); // to update the existing goal
+            return goalToUpdate.GetPoints();
+        }
+        else
+        {
+            Console.WriteLine("Invalid goal index. Please try again.");
+            return 0;
+        }
     }
-    else
-    {
-        Console.WriteLine("Invalid goal index. Please try again.");
-        return 0;
-    }
-}
 }
