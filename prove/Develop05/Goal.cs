bc8ddf7e67c84aa4ef6aa436e9c7d6e2994a3c65
joy935 <tupaiv@gmail.com>
2023-11-17 12:22:42 +0100

    added comments

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,13 +1,18 @@
 using System;
 
 /* Goal Base Class */
-public abstract class Goal
+public abstract class Goal          // abstract class because some methods are not instantiated
 {
-    protected string _type;
-    protected string _name;
-    protected string _description;
-    protected int _points;
+    // attributes of the class
+    protected string _type;         // the type of the goal
+    protected string _name;         // the name of the goal
+    protected string _description;  // the description of the goal
+    protected int _points;          // the points associated with the goal
 
+    /* Goal is a constructor method that creates an instance of the Goal class.
+    Parameters: string type, string name, string description, int points
+    Return: none
+    */
     public Goal(string type, string name, string description, int points)
     {
         _type = type;
@@ -15,7 +20,7 @@ public abstract class Goal
         _description = description;
         _points = points;
     }
-
+    /* Getters and setters for the attributes of the class */
     public string GetGoalType()
     {
         return _type;
@@ -56,6 +61,11 @@ public abstract class Goal
         _points = points;
     }
 
+    /* CreateGoal is a virtual method that creates a new goal
+    It has some implemmentation in the base class but it can be overriden in the derived classes
+    Parameters: none
+    Return: none
+    */
     public virtual void CreateGoal()
     {
         Console.Write("What is the name of your goal? ");
@@ -66,11 +76,38 @@ public abstract class Goal
         _points = Convert.ToInt32(Console.ReadLine());
     }
 
+    /* ListGoals is an abstract method that lists the goals
+    It has no implementation in the base class and it must be overriden 
+    in the derived classes
+    Parameters: none
+    Return: none
+    */
     public abstract void ListGoals();
 
+    /* GetStringRepresentation is an abstract method that returns a string 
+    representation of the goal
+    It has no implementation in the base class and it must be overriden
+    in the derived classes
+    Parameters: none
+    Return: string, the string representation of the goal
+    */
     public abstract string GetStringRepresentation();
 
+    /* IsCompleted is an abstract method that returns true if the goal 
+    is completed and false if the goal is not completed
+    It has no implementation in the base class and it must be overriden
+    in the derived classes
+    Parameters: none
+    Return: bool, true if the goal is completed and false if the goal 
+    is not completed
+    */
     public abstract bool IsCompleted();
 
+    /* RecordEvent is an abstract method that records an event
+    It has no implementation in the base class and it must be overriden
+    in the derived classes
+    Parameters: none
+    Return: int, the number of points associated with the goal
+    */
     public abstract int RecordEvent();
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,12 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Goal> goals = new List<Goal>();
-        LoadSaveGoal lsGoals = new LoadSaveGoal("filename");
-        UserInterface userInterface = new UserInterface(goals);
-        int totalPoints = 0;
-        Game game = new Game(totalPoints);
+    
+        List<Goal> goals = new List<Goal>();                    // Create a list of Goal objects
+        LoadSaveGoal lsGoals = new LoadSaveGoal("filename");    // Create a LoadSaveGoal object
+        UserInterface userInterface = new UserInterface(goals); // Create a UserInterface object
+        int totalPoints = 0;                                    // Create a totalPoints variable and set the value to 0
+        Game game = new Game(totalPoints);                      // Create a Game object
 
+        // Display the menu options and loop until the user chooses to quit
         while (true)
         {
             Console.WriteLine($"\nYou have {totalPoints} points.\n");
@@ -21,69 +23,86 @@ class Program
             Console.WriteLine(" 5. Record Event");
             Console.WriteLine(" 6. Quit");
             Console.Write("Select a choice from the menu: ");
+        
+            int choice = int.Parse(Console.ReadLine()); // Get the user's choice and convert it to an integer
 
-            int choice = int.Parse(Console.ReadLine());
-
+            // if the user chooses to create a new goal
             if (choice == 1)
             {
+                // Display the goal types options
                 Console.WriteLine("The type of goals are: ");
                 Console.WriteLine("1. Simple Goal");
                 Console.WriteLine("2. Eternal Goal");
                 Console.WriteLine("3. Checklist Goal");
                 Console.WriteLine("Which type of goal would you like to create? ");
-                int goalType = int.Parse(Console.ReadLine());
 
+                int goalType = int.Parse(Console.ReadLine()); // Get the user's choice and convert it to an integer
+
+                // if the user chooses to create a simple goal
                 if (goalType == 1)
                 {
+                    // create a new simple goal object
                     SimpleGoal sGoal = new SimpleGoal("simple", "new", "description", 0, false);
+                    // call the CreateGoal method
                     sGoal.CreateGoal();
+                    // add the goal to the list of goals
                     goals.Add(sGoal);
                 }
+                // if the user chooses to create an eternal goal
                 else if (goalType == 2)
                 {   
                     EternalGoal eGoal = new EternalGoal("eternal", "new", "description", 0);
                     eGoal.CreateGoal();
                     goals.Add(eGoal);
                 }
+                // if the user chooses to create a checklist goal
                 else if (goalType == 3)
                 {   
                     ChecklistGoal cGoal = new ChecklistGoal("checklist", "new", "description", 0, 0, 0, 0);
                     cGoal.CreateGoal();
                     goals.Add(cGoal);
                 }
+                // if the user chooses an invalid option
                 else
                 {
                     Console.WriteLine("Invalid choice. Please try again.");
                 }
             }
+            // if the user chooses to list the goals
             else if (choice == 2)
             {
                 Console.WriteLine("The goals are: ");
-                int i = 1;
+                int i = 1; // create a counter variable
+                // loop through the list of goals and display them
                 foreach (Goal goal in goals)
                 {
                     Console.Write($"{i}. ");
                     goal.ListGoals();
                     i++;
                 }
+                // display the current level
                 game.DisplayLevel();
             }
+            // if the user chooses to save the goals
             else if (choice == 3) // save goals
             {
                 Console.Write("What is the filename for the goal file? ");
-                string fileName = Console.ReadLine();
-                lsGoals.SaveGoal(fileName, goals);
+                string fileName = Console.ReadLine();   // get the filename from the user
+                lsGoals.SaveGoal(fileName, goals);      // call the SaveGoal method
 
             }
-            else if (choice == 4) // load goals
+            // if the user chooses to load the goals
+            else if (choice == 4)
             {
                 Console.Write("What is the filename for the goal file? ");
-                string fileName = Console.ReadLine();
-                goals = lsGoals.LoadGoal(fileName);
-                totalPoints = lsGoals.GetTotalPoints();
+                string fileName = Console.ReadLine();   // get the filename from the user
+                goals = lsGoals.LoadGoal(fileName);     // call the LoadGoal method
+                totalPoints = lsGoals.GetTotalPoints(); // get the total points from the file
             }
+            // if the user chooses to record an event
             else if (choice == 5)
             {
+                // display the goals
                 Console.WriteLine("The goals are: ");
                 int i = 1;
                 foreach (Goal goal in goals)
@@ -92,18 +111,21 @@ class Program
                     goal.ListGoals();
                     i++;
                 }
-                Console.Write("Which goal did you accomplish? ");
-                int goalDone = int.Parse(Console.ReadLine()) - 1;
-                int points = userInterface.UpdateGoal(goalDone, goals);
-                totalPoints += points;
-                lsGoals.SetTotalPoints(totalPoints);
-                game.SetTotalPoints(totalPoints);
-                game.LevelUp();
+                Console.Write("Which goal did you accomplish? ");       
+                int goalDone = int.Parse(Console.ReadLine()) - 1;       // get the goal completed from the user and convert it to an integer
+                int points = userInterface.UpdateGoal(goalDone, goals); // call the UpdateGoal method
+                totalPoints += points;                                  // add the points to the total points
+                lsGoals.SetTotalPoints(totalPoints);                    // set the total points in the file
+                game.SetTotalPoints(totalPoints);                       // set the total points in the game object
+                game.LevelUp();                                         // call the LevelUp method
             }
+            // if the user chooses to quit
             else if (choice == 6)
             {
+                // break the loop
                 break;
             }
+            // if the user chooses an invalid option
             else 
             {
                 Console.WriteLine("Invalid choice. Please try again.");
