87bd46fa317fbb980f2274f9ee42bd781cea3ad9
joy935 <tupaiv@gmail.com>
2023-11-24 16:49:32 +0100

    added attribute and constructor

--- final/FinalProject/UserInterface.cs
+++ final/FinalProject/UserInterface.cs
@@ -2,5 +2,12 @@ using System;
 
 /* User Interface Class */
 class UserInterface {
-    
+
+    /* attribute of the class */
+    private List<Tasks> _tasks; // list of tasks
+
+    /* constructor of the class */
+    public UserInterface() {
+        _tasks = new List<Tasks>();
+    }
 }
