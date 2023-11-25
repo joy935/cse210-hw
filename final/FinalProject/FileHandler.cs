d0eea705aa0798f2136bec106e432d134db3a633
joy935 <tupaiv@gmail.com>
2023-11-25 15:50:29 +0100

    added constructor

--- final/FinalProject/FileHandler.cs
+++ final/FinalProject/FileHandler.cs
@@ -6,4 +6,12 @@ public class FileHandler
     /* attributes of the class */
     private string _fileName;    // name of the file
     private List<Task> _tasks;   // list of tasks
+
+    /* constructor of the class */
+    public FileHandler(string fileName, List<Task> tasks)
+    {
+        _fileName = fileName;
+        _tasks = tasks;
+    }
+    
 }
