7be8d13ab124b96e1383e285fcd16d2805a1df0d
joy935 <tupaiv@gmail.com>
2023-11-25 15:48:54 +0100

    created FileHandler class

---
+++ final/FinalProject/FileHandler.cs
@@ -0,0 +1,9 @@
+using System;
+
+/* FileHandler Class */
+public class FileHandler
+{
+    /* attributes of the class */
+    private string _fileName;    // name of the file
+    private List<Task> _tasks;   // list of tasks
+}
