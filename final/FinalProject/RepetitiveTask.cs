2061ade2dea7f51387fb45f5bbf9d1a0e7a0f15e
joy935 <tupaiv@gmail.com>
2023-11-28 12:44:37 +0100

    changed type of frequency

--- final/FinalProject/RepetitiveTask.cs
+++ final/FinalProject/RepetitiveTask.cs
@@ -4,13 +4,13 @@ using System;
 public class RepetitiveTask : Task
 {
     /* attributes of the class */
-    private int _frequencyRepetition;   // frequency of the task
+    private string _frequencyRepetition;   // frequency of the task
     private DateTime _dueDate;          // first due date of the task
     private int _numberRepetition;      // number of times the task has been repeated
     private int _totalRepetition;       // total number of times the task should be repeated
 
     /* constructor of the class */
-    public RepetitiveTask(string taskType, string taskDescription, bool isCompleted, int frequencyRepetition, DateTime dueDate, int numberRepetition, int totalRepetition) : base(taskType, taskDescription, isCompleted)
+    public RepetitiveTask(string taskType, string taskDescription, bool isCompleted, string frequencyRepetition, DateTime dueDate, int numberRepetition, int totalRepetition) : base(taskType, taskDescription, isCompleted)
     {
         _frequencyRepetition = frequencyRepetition;
         _dueDate = dueDate;
@@ -19,11 +19,11 @@ public class RepetitiveTask : Task
     }
 
     /* Getters and setters for the attributes of the class */
-    public int GetFrequencyRepetition()
+    public string GetFrequencyRepetition()
     {
         return _frequencyRepetition;
     }
-    public void SetFrequencyRepetition(int frequencyRepetition)
+    public void SetFrequencyRepetition(string frequencyRepetition)
     {
         _frequencyRepetition = frequencyRepetition;
     }
