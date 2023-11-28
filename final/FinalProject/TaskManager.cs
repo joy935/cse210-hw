c1f18ed6e8ec28590914716faa15130c3bd035bc
joy935 <tupaiv@gmail.com>
2023-11-28 10:35:13 +0100

    added getters and setters

--- final/FinalProject/TaskManager.cs
+++ final/FinalProject/TaskManager.cs
@@ -13,5 +13,24 @@ public class TaskManager
         _fileName = fileName;
         _tasks = tasks;
     }
+
+    /* Getters and setters for the attributes of the class */
+    public string GetFileName()
+    {
+        return _fileName;
+    }
+    public void SetFileName(string fileName)
+    {
+        _fileName = fileName;
+    }
+    public List<Task> GetTasks()
+    {
+        return _tasks;
+    }
+    public void SetTasks(List<Task> tasks)
+    {
+        _tasks = tasks;
+    }
+
     
 }
