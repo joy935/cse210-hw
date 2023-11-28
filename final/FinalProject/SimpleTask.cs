9dfa745300899e4654aca312831bf8ad9cf4de69
joy935 <tupaiv@gmail.com>
2023-11-28 20:03:35 +0100

    added description of the methods

--- final/FinalProject/SimpleTask.cs
+++ final/FinalProject/SimpleTask.cs
@@ -10,7 +10,13 @@ public class SimpleTask : Task
     {}
 
     /* methods of the class */
-    /* GetTaskInfo ...
+    /* GetTaskInfo is a method that returns the task info 
+    in a specific format to be saved later in a file.
+    If the task is completed, the method returns the task info
+    with the isCompleted attribute set to true. Otherwise, it returns
+    the task info with the isCompleted attribute set to false.
+    Parameters: none
+    Return: string
     */
     public override string GetTaskInfo()
     {
@@ -24,7 +30,9 @@ public class SimpleTask : Task
         }
     }
 
-    /* DisplayTaskInfo ...
+    /* DisplayTaskInfo is a method that displays the task info.
+    Parameters: none
+    Return: none
     */
     public override void DisplayTaskInfo()
     {
