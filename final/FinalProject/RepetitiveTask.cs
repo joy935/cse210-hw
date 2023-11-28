bd6d3a167edc90254c560ee82c73654dad8fbac7
joy935 <tupaiv@gmail.com>
2023-11-28 20:05:46 +0100

    added decription of the methods

--- final/FinalProject/RepetitiveTask.cs
+++ final/FinalProject/RepetitiveTask.cs
@@ -4,10 +4,10 @@ using System;
 public class RepetitiveTask : Task
 {
     /* attributes of the class */
-    private string _frequencyRepetition;   // frequency of the task
-    private DateOnly _dueDate;          // first due date of the task
-    private int _numberRepetition;      // number of times the task has been repeated
-    private int _totalRepetition;       // total number of times the task should be repeated
+    private string _frequencyRepetition;    // frequency of the task
+    private DateOnly _dueDate;              // first due date of the task
+    private int _numberRepetition;          // number of times the task has been repeated
+    private int _totalRepetition;           // total number of times the task should be repeated
 
     /* constructor of the class */
     public RepetitiveTask(string taskType, string taskDescription, bool isCompleted, string frequencyRepetition, DateOnly dueDate, int numberRepetition, int totalRepetition) : base(taskType, taskDescription, isCompleted)
@@ -52,7 +52,14 @@ public class RepetitiveTask : Task
         _totalRepetition = totalRepetition;
     }
 
-    /* GetTaskInfo ...
+    /* methods of the class */
+    /* GetTaskInfo is a method that returns the task info
+    in a specific format to be saved later in a file.
+    If the task is completed, the method returns the task info
+    with the isCompleted attribute set to true. Otherwise, it returns
+    the task info with the isCompleted attribute set to false.
+    Parameters: none
+    Return: string
     */
     public override string GetTaskInfo()
     {
@@ -66,7 +73,9 @@ public class RepetitiveTask : Task
         }
     }
 
-    /* DisplayTaskInfo ...
+    /* DisplayTaskInfo is a method that displays the task info.
+    Parameters: none
+    Return: none
     */
     public override void DisplayTaskInfo()
     {
