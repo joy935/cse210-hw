fb105133d7f0017cbbdb0ff46a239dc5244dcc60
joy935 <tupaiv@gmail.com>
2023-12-05 10:48:11 +0100

    added description

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Task> tasks = new List<Task>();
-        UserInterface userInterface = new UserInterface(tasks);
+        List<Task> tasks = new List<Task>(); // create a new list of tasks
+        UserInterface userInterface = new UserInterface(tasks); // create a new object of the UserInterface class
 
         Console.Clear(); // clear the screen
         Console.WriteLine("Welcome to the To-Do List Application!"); // welcome message
